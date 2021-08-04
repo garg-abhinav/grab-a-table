f3673d4943a7b8874a61853acf4ed40d46126970
mohit-mhjn <mohitm3@illinois.edu>
2021-08-03 19:06:06 -0500

    deployed_to_production

--- arrowheads/settings.py
+++ arrowheads/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7+18)g829=v%g-*o=!1q*bdxxa0l#$jyg$*c&ru#1=(-!$a-e^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False;
 
-ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]', ]
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]','cs411-database-systems.uc.r.appspot.com']
 
 
 # Application definition
